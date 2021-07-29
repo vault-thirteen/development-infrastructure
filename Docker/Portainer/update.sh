0500e0afae05bf6d5f6c14db7d00d3bc37e8ff88
Vault Thirteen <legacy-vault@yandex.com>
2021-07-29 13:47:03 +0300

    ...

--- Docker/Portainer/update.sh
+++ Docker/Portainer/update.sh
@@ -11,7 +11,13 @@ docker rm portainer
 docker pull portainer/portainer-ce
 
 # Portainer Start.
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d \
+  -p 8000:8000 \
+  -p 9000:9000 \
+  --name=portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data portainer/portainer-ce
 
 # Source:
 # https://documentation.portainer.io/v2.0/upgrade/upddocker/
